f8b65e21a0de75a262aee745cc36239740ea381e
H0meDev <362397585@qq.com>
2014-12-29 09:38:19 +0800

    Create RequestOperation.h

---
+++ RequestOperation.h
@@ -0,0 +1,20 @@
+//
+//  RequestOperation.h
+//  NetworkSDK
+//
+//  Created by Cailiang on 14-9-16.
+//  Copyright (c) 2014年 Cailiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CHTTPRequest.h"
+
+@interface CHTTPOperation : NSOperation
+
+- (id)initWithRequest:(NSURLRequest *)request callback:(RequestCallback)callback;
+- (id)initWithRequest:(NSURLRequest *)request delegate:(id<CHTTPRequestDelegate>)delegate tag:(int)tag;
+
+// Set timeout, default is 30s
+- (void)setTimeOut:(NSInteger)seconds;
+
+@end
